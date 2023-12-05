97b9913944f57435528b92cac882be7d7b08a90c
satheesh <satheeshkmr955@gmail.com>
2023-12-05 23:54:23 +0530

    completed feed page

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const url = "/api/prompt";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+const PromptCardList = (props) => {
+  const { data, handleTagClick } = props;
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => {
+        return (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Feed;
